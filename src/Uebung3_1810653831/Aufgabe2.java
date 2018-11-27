9700692ec68cac5fa7be076417760da093de741c
DorferThomas <1810653831@stud.fh-kufstein.ac.at>
2018-11-27 22:25:54 +0100

    Aufgabe3Taschenrechner

--- src/Uebung3_1810653831/Aufgabe2.java
+++ src/Uebung3_1810653831/Aufgabe2.java
@@ -3,11 +3,20 @@ package Uebung3_1810653831;
 public class Aufgabe2 {
     public static void main(String[] args)
     {
+        //Aufgabe2:
         //Instanz wird angelegt, damit die nicht Static Methode aufgerufen werden kann
         Aufgabe2 eingabe = new Aufgabe2();
         eingabe.partialString("hi");
         eingabe.partialString("Dieser String ist 60 Stellen lang, geteilt durch 2 ergibt:  ");
+
+        //Aufgabe3:(Taschenrechner)
+        Taschenrechner rechnen = new Taschenrechner();
+        System.out.println("Ergebnis der Addition: " + rechnen.addieren(12,23));
+        System.out.println("Ergebnis der Subdraktion: " + rechnen.subdrahieren(88,333));
+        System.out.println("Ergebnis der Division: " + rechnen.division(4566.344,23.33));
+        System.out.println("Ergebnis der Multiplikation: " + rechnen.multiplikation(0.33,100));
     }
+
     public void partialString(String a)
     {
         if (a.length()<=3)              //.length() überprüft, wie lange der string ist.(nur bei String)
---
+++ src/Uebung3_1810653831/Taschenrechner.java
@@ -0,0 +1,26 @@
+package Uebung3_1810653831;
+
+public class Taschenrechner {
+    //Erstellen Sie eine neue Klasse Taschenrechner in Ihrem Projekt. Diese soll vier Methoden für die
+    //Grundrechenarten enthalten(jeweils zwei Variablen vom jeweiligen Typ als Parameter). Es sollen
+    //sowohl ganzzahlige, als auch Gleitkomma Operationen möglich sein. Rufen Sie dann aus Ihrer main
+    //Methode (Aufgabe 2) alle Methoden jeweils einmal auf und geben Sie das Ergebnis auf der Konsole
+    //aus.
+
+    public double addieren(double a, double b)
+    {
+        return a + b;
+    }
+    public double subdrahieren(double a, double b)
+    {
+        return a - b;
+    }
+    public double division(double a, double b)
+    {
+        return a / b;
+    }
+    public double multiplikation(double a, double b)
+    {
+        return a * b;
+    }
+}
